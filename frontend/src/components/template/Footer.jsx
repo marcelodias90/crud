b2ebbb4cc7673c05dcef247af89b1d8237041d73
Marcelo Dias <diasteixeira1990@gmail.com>
2022-12-21 14:28:55 -0300

    frontend

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Desenvolvido por <a href="https://www.linkedin.com/in/marcelo-dias-b098ab229/"> Marcelo<span className='text-danger'> D</span>ias</a>
+        </span>
+    </footer>
